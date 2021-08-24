e420da760ba929b4d7e5d7e0dbf306357341848e
AlexV <versh2330@gmail.com>
2021-08-24 15:09:20 +0300

    fixing the hwk

--- script.js
+++ script.js
@@ -34,23 +34,12 @@ if (typeof window !== "undefined") {
         privat: false
     };
 
-    let answerOne;
-    answerOne = askQuestions();
-    personalMovieDB.movies = answerOne;
-    console.log(personalMovieDB.movies);
+   let a = prompt('Один из последних просмотренных фильмов?', '');
+   let b = prompt('На сколько оцените его?','');
+   let c = prompt('Один из последних просмотренных фильмов?', '');
+   let d = prompt('На сколько оцените его?','');
 
-    let answerTwo;
-    answerTwo = askQuestions();
-    personalMovieDB.movies = answerTwo;
-    console.log(personalMovieDB.movies);
-}
-
-function askQuestions() {
-
-    let filmName = window.prompt("Один из последних просмотренных фильмов?", "");
-    let filmRating = window.prompt("Rating", "0.0");
-    let answer = {
-        [filmName]: filmRating
-    };
-    return answer;
+   personalMovieDB.movies[a] = b;
+   personalMovieDB.movies[c] = d;
+   console.log(personalMovieDB);
 }
